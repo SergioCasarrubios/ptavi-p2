04a7319e1098b7a32b26ab9d95c1ba5cc5b32446
Sergio Casarrubios <scasarrubios@virtualbox.org>
2016-09-20 20:23:42 +0200

    calcoohija terminado

---
+++ calcoohija.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+class Calculadora():
+    
+    def suma(self, op1, op2):
+        return op1 + op2 
+    
+    def resta(self, op1, op2):
+        return op1 - op2
+        
+class CalculadoraHija(Calculadora):
+    def mult(self, op1, op2):
+        return op1 * op2
+    
+    def divide(self, op1, op2):
+        try:
+            return op1 / op2 
+        except ZeroDivisionError:
+            sys.exit("Division by zero is not allowed")        
+
+if __name__ == "__main__":
+    
+    calculadora = CalculadoraHija()
+    try:
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
+    
+    if sys.argv[2] == 'suma':
+        result = calculadora.suma(operando1, operando2)
+    elif sys.argv[2] == 'resta':
+        result = calculadora.resta(operando1, operando2)
+    elif sys.argv[2] == 'multiplica':
+        result = calculadora.mult(operando1, operando2)
+    elif sys.argv[2] == 'divide':
+        result = calculadora.divide(operando1, operando2)
+    else:
+        sys.exit('Operación no definida.')
+    print(result)
