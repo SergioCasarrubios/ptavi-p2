8a2ecba9f55c35cb3602f0e7de71c6869423608e
Sergio Casarrubios <scasarrubios@virtualbox.org>
2016-09-25 23:55:12 +0200

    normas de pep8 aplicadas

--- calcplus.py
+++ calcplus.py
@@ -7,9 +7,9 @@ import calcoohija
 fichero = open(sys.argv[1], 'r')
 calculadora = calcoohija.CalculadoraHija
 lineas = fichero.readlines()
-funciones = {'suma': calculadora.suma, 'resta': calculadora.resta, 
-                'multiplica': calculadora.mult, 'divide': calculadora.divide}
-    
+funciones = {'suma': calculadora.suma, 'resta': calculadora.resta,
+             'multiplica': calculadora.mult, 'divide': calculadora.divide}
+
 for linea in lineas:
     datos = linea.split(',')
     operacion = datos[0]
@@ -17,12 +17,10 @@ for linea in lineas:
     datos[-1] = datos[-1][:-1]
     result = int(datos[0])
     datos = datos[1:]
-    
+
     for dato in datos:
         try:
             result = funciones[operacion](0, result, int(dato))
         except KeyError:
             sys.exit('Operación no definida.')
     print(result)
-            
-    
